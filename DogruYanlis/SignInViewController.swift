83b30a94e55e40eff12079849e3cc16b72fbd295
Başar Oğuz <baoguz@ku.edu.tr>
2017-01-25 19:40:48 +0200

    Before connecting the Sign In Screen to the Game View Controller.

--- DogruYanlis/SignInViewController.swift
+++ DogruYanlis/SignInViewController.swift
@@ -17,7 +17,27 @@ class SignInViewController: UIViewController, UITextFieldDelegate {
         super.viewDidLoad()
         
         ref = FIRDatabase.database().reference()
+        
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(SignInViewController.dismissKeyboard))
+        view.addGestureRecognizer(tap)
+        
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        
+        super.viewWillAppear(animated)
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(SignInViewController.keyboardWillShowNotification(_:)), name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(SignInViewController.keyboardWillHideNotification(_:)), name: UIKeyboardWillHideNotification, object: nil)
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        
+        super.viewWillDisappear(animated)
+        
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardWillHideNotification, object: nil)
     }
 
     override func didReceiveMemoryWarning() {
@@ -37,12 +57,78 @@ class SignInViewController: UIViewController, UITextFieldDelegate {
     */
     @IBOutlet weak var gameNameField: UITextField!
     
+    private var gameName: String {
+        get {
+            if (gameNameField.text != nil) {
+                return (gameNameField.text?.lowercaseString)!
+            }
+            else {
+                return ""
+            }
+        }
+    }
     
+    @IBOutlet weak var userNameField: UITextField!
     
-    
+
     @IBAction func newGame(sender: UIButton) {
+        
+        ref.child("sessions").observeSingleEventOfType(.Value, withBlock: {
+            (snapshot) in
+            
+            if snapshot.hasChild(self.gameName){
+                
+                print("Game already exists. We are directing you to the game!")
+                
+            } else {
+                print(self.gameName)
+                self.ref.child("sessions/\(self.gameName)/users/name").setValue(self.userNameField.text!.lowercaseString)
+                
+            }
+        })
+
+    }
+    
+    @IBAction func joinGame(sender: UIButton) {
+        self.ref.child("sessions/\(gameNameField.text!)/users/")
+    }
+    
+    func joinGameWithName(gameName: String) {
         
         
     }
 
+    @IBOutlet weak var bottomConstraint: NSLayoutConstraint!
+    
+    
+    func keyboardWillShowNotification(notification: NSNotification) {
+        
+        let info = notification.userInfo!
+        let keyboardFrame: CGRect = (info[UIKeyboardFrameEndUserInfoKey] as! NSValue).CGRectValue()
+        
+        UIView.animateWithDuration(0.1, animations: { () -> Void in
+            
+            self.bottomConstraint.constant = -keyboardFrame.size.height
+            self.view.layoutIfNeeded()
+
+        })
+
+    }
+    
+    func keyboardWillHideNotification(notification: NSNotification) {
+        
+        UIView.animateWithDuration(0.1, animations: { () -> Void in
+            
+            self.bottomConstraint.constant = 0
+            self.view.layoutIfNeeded()
+            
+        })
+        
+    }
+    
+    func dismissKeyboard() {
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
+    }
+    
 }
